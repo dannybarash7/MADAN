1a5f0839567ec85f7d0c3f315a4e8fc50bb825b6
dannybarash7 <dannybarash7@gmail.com>
2020-08-31 23:36:05 +0000

    m

--- scripts/CycleGAN/cyclegan_gta_synthia2cityscapes.sh
+++ scripts/CycleGAN/cyclegan_gta_synthia2cityscapes.sh
@@ -6,7 +6,7 @@ python3 train.py --name cyclegan_gta_synthia2cityscapes_noIdentity \
     --model multi_cycle_gan_semantic --no_flip --batchSize 4 \
     --dataset_mode gta_synthia_cityscapes --dataroot /data/images \
      --DSC --general_semantic_weight 20 --CCD --SAD --CCD_weight 0.2 --SAD_frozen_epoch 5 --CCD_frozen_epoch 10 --max_epoch 40 \
-    --weights_syn /users/danny.barash/MADAN/pretrained_models/cyclesynthia_drn26_iter2000.pth \
+    --weights_syn /users/danny.barash/MADAN/pretrained_models/drn26_cycada_synthia2cityscapes.pth \
     --weights_gta /users/danny.barash/MADAN/pretrained_models/drn26_cycada_cyclegta2cityscapes.pth \
     --gpu 0,1,2,3 --semantic_loss
 
